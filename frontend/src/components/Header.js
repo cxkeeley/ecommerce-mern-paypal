cf0abde63683e47dbd5ba1b453293aea031248a5
cxkeeley <cxkeeley@gmail.com>
2022-06-20 22:04:57 +0700

    commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Route} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {LinkContainer} from 'react-router-bootstrap'
+import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+
+        
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
---
+++ frontend/src/store.js
@@ -0,0 +1,4 @@
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+// import { composeWithDevTools } from 'redux-devtools-extension'
+// import { productListReducer, productDetailsReducer, productDeleteReducer, productCreateReducer, productUpdateReducer, productReviewCreateReducer, productTopRatedReducer } from './'
